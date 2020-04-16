91287b314955535a25d012f0a35d10b8db7ade0c
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-04-16 14:05:58 +0300

    Added skeleton AkkaHttp skeleton application that will be used as base for rtb agent.

---
+++ src/main/scala/small/rtb/agent/Application.scala
@@ -0,0 +1,40 @@
+package small.rtb.agent
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.adapter._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+
+import scala.util.{Failure, Success}
+
+object Application {
+  private def startHttpServer(routes: Route, system: ActorSystem[_]): Unit = {
+    implicit val classicSystem: akka.actor.ActorSystem = system.toClassic
+    import system.executionContext
+
+    val futureBinding = Http().bindAndHandle(routes, "localhost", 8080)
+
+    futureBinding.onComplete {
+      case Success(binding) =>
+        val address = binding.localAddress
+        system.log.info("Server online at http://{}:{}/", address.getHostString, address.getPort)
+      case Failure(ex) =>
+        system.log.error("Failed to bind HTTP endpoint, terminating system", ex)
+        system.terminate()
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val rootBehavior = Behaviors.setup[Nothing] { context =>
+      val userRegistryActor = context.spawn(UserRegistry(), "UserRegistryActor")
+      context.watch(userRegistryActor)
+
+      val routes = new UserRoutes(userRegistryActor)(context.system)
+      startHttpServer(routes.userRoutes, context.system)
+
+      Behaviors.empty
+    }
+    val system = ActorSystem[Nothing](rootBehavior, "SimpleRtbAgentHttpServer")
+  }
+}
---
+++ src/main/scala/small/rtb/agent/UserRoutes.scala
@@ -0,0 +1,62 @@
+package small.rtb.agent
+
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
+
+import scala.concurrent.Future
+import small.rtb.agent.UserRegistry._
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
+import small.rtb.agent.UserRegistry.{ActionPerformed, GetUserResponse, GetUsers}
+
+class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val system: ActorSystem[_]) {
+  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+  import JsonFormats._
+
+  private implicit val timeout = Timeout.create(system.settings.config.getDuration("small-rtb-agent-app.routes.ask-timeout"))
+
+  def getUsers(): Future[Users] =
+    userRegistry.ask(GetUsers)
+  def getUser(name: String): Future[GetUserResponse] =
+    userRegistry.ask(GetUser(name, _))
+  def createUser(user: User): Future[ActionPerformed] =
+    userRegistry.ask(CreateUser(user, _))
+  def deleteUser(name: String): Future[ActionPerformed] =
+    userRegistry.ask(DeleteUser(name, _))
+
+  val userRoutes: Route =
+    pathPrefix("users") {
+      concat(
+        pathEnd {
+          concat(
+            get {
+              complete(getUsers())
+            },
+            post {
+              entity(as[User]) { user =>
+                onSuccess(createUser(user)) { performed =>
+                  complete((StatusCodes.Created, performed))
+                }
+              }
+            })
+        },
+        path(Segment) { name =>
+          concat(
+            get {
+              rejectEmptyResponse {
+                onSuccess(getUser(name)) { response =>
+                  complete(response.maybeUser)
+                }
+              }
+            },
+            delete {
+              onSuccess(deleteUser(name)) { performed =>
+                complete((StatusCodes.OK, performed))
+              }
+            })
+        })
+    }
+}
